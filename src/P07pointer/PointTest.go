7ea0852c6e1793e1008d51200f3fb4e33e0eb490
liukai <forfreeday@163.com>
2021-06-17 18:10:52 +0800

    添加引用

--- src/P07pointer/PointTest.go
+++ src/P07pointer/PointTest.go
@@ -16,3 +16,23 @@ func PointTest01() {
 	/* 使用指针访问值 */
 	fmt.Printf("*ip 变量的值: %d\n", *ip)
 }
+
+//测试引用传递
+func PointTest02() {
+	var a int = 100
+	var b int = 200
+
+	fmt.Printf("交换前: a 的值： %d\n\n", a)
+	fmt.Printf("交换前: b 的值： %d\n\n", b)
+	swap(&a, &b)
+
+	fmt.Printf("交换后: a 的值： %d\n\n", a)
+	fmt.Printf("交换后: b 的值： %d\n\n", b)
+}
+
+func swap(x *int, y *int) {
+	var temp int
+	temp = *x /* 保存 x 地址上的值 */
+	*x = *y   /* 将 y 值赋给 x */
+	*y = temp /* 将 temp 值赋给 y */
+}
--- src/P07pointer/main/main.go
+++ src/P07pointer/main/main.go
@@ -3,5 +3,6 @@ package main
 import "P07pointer"
 
 func main() {
-	P07pointer.PointTest01()
+	//P07pointer.PointTest01()
+	P07pointer.PointTest02()
 }
