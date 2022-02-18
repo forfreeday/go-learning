72f5d15e8b3118a6a16ecc359878d5cb31aefab7
liukai <forfreeday@163.com>
2022-02-19 01:50:29 +0800

    优化 readme

---
+++ src/p01var/main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "golearning/src/p01var"
+
+func main() {
+	Chart01Test()
+}
+
+func Chart01Test() {
+	p01var.VarTest01()
+	//p01var.VarTest02()
+	//p01var.VarTest03()
+	//p01var.VarTest04()
+	//p01var.SwitchTest()
+
+	//p01var.ArrayTest()
+	//p01var.ArrayTest2()
+	//p01var.ArrayTest3()
+	//p01var.ArrayTest4()
+	//p01var.ArrayTest5()
+	//p01var.ArrayTest6()
+
+	//var i interface{}
+	//p01var.SwitchTest2(i)
+	//p01var.SwitchTest2(21)
+	//p01var.SwitchTest2("hello")
+	//p01var.SwitchTest2(true)
+
+	//p01var.SwitchTest()
+	//p01var.ForLoopTest()
+	//p01var.ForLoopTest()
+}
